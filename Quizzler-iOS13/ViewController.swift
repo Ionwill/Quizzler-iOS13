48dc8114518b1ce49fc3d55d4c3327f907a62d8f
Ayan Williams <101142116+Ionwill@users.noreply.github.com>
2024-11-30 14:18:43 -0600

    lesson 9.96

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var questionNumber = 0
+    
+    //create an array of quiz questions
+    let quiz = [
+        "4 + 2 = 6",
+        "5 - 3 > 1",
+        "3 + 8 < 10"
+        ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        questionNumber = 0
+        
+        updateUI()
     }
-
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        //variable that keeps track of which question the user is reading
+        questionNumber += 1
+        print(questionNumber)
+        
+        //Since this is in the button pressed function it updates the question everytime the button is pressed
+        updateUI()
+    
+    }
+    
+    //function to update question with new questionNumber
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber]
+    }
+    
 
 }
 
+
+/*
+ 
+ variable that gives user a random question
+    questionNumber = Int.random(in: 0...2)
+ 
+ 
+ */
