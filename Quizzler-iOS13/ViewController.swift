78e63138f06d4f47536192a88cb9c1925819f304
Ayan Williams <101142116+Ionwill@users.noreply.github.com>
2024-12-02 11:21:59 -0600

    Using structures in Quizzler app
    Changed 2D array to structures and created a new file just for the structure

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -19,9 +19,19 @@ class ViewController: UIViewController {
     
     //create an array of quiz questions
     let quiz = [
-        "4 + 2 = 6",
-        "5 - 3 > 1",
-        "3 + 8 < 10"
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+
         ]
     
     
@@ -37,9 +47,28 @@ class ViewController: UIViewController {
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
+        //True or False
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber].answer
+        
+        if userAnswer == actualAnswer{
+            print("Right")
+        }else{
+            print("Wrong")
+        }
+        
+        
+        if questionNumber < quiz.count - 1 {
         //variable that keeps track of which question the user is reading
         questionNumber += 1
         print(questionNumber)
+        }else{
+            print("Quiz done")
+            questionNumber = 0
+        }
+        
+        
+        
         
         //Since this is in the button pressed function it updates the question everytime the button is pressed
         updateUI()
@@ -48,7 +77,7 @@ class ViewController: UIViewController {
     
     //function to update question with new questionNumber
     func updateUI(){
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = quiz[questionNumber].text
     }
     
 
